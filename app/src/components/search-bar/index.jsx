42dde3dfa294532056eb26b1172e176e02ba3d10
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-31 14:04:07 +0300

    Static application without logic

---
+++ app/src/components/search-bar/index.jsx
@@ -0,0 +1,26 @@
+export default function SearchBar({
+    filterText,
+    inWhishListOnly,
+    onFilterTextChange,
+    oninWhishListOnlyChange,
+}) {
+    return (
+        <form className="filter-form">
+            <input
+                className="search-input"
+                type="text"
+                value={filterText}
+                placeholder="Search by name"
+                onChange={(e) => onFilterTextChange(e.target.value)}
+            />
+            <label>
+                <input
+                    type="checkbox"
+                    checked={inWhishListOnly}
+                    onChange={(e) => oninWhishListOnlyChange(e.target.checked)}
+                />
+                Only on the wish list
+            </label>
+        </form>
+    );
+}
